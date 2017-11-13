22f3f1b74dd099096329ba47b35a48fb8c724433
mauroianni <mauroianni@gmail.com>
2017-11-13 13:41:15 +0100

    Inserito codice di debug nella versione a 4lvl: attenzione, la soluzione non ancora stabile

--- 4lvl/correctness.c
+++ 4lvl/correctness.c
@@ -10,9 +10,9 @@
 #include "utils.h"
 
 #ifdef NUMA
-    #include <numa.h>
-    #include <numaif.h>
-    #include <sched.h>
+	#include <numa.h>
+	#include <numaif.h>
+	#include <sched.h>
 #endif
 
 #ifdef NUMA
@@ -32,304 +32,311 @@ unsigned long long *volatile failures, *volatile allocs, *volatile frees, *volat
 
 
 void parallel_try(){
-    unsigned int i, j, tentativi, kept;
-    unsigned long scelta;
-    unsigned int scelta_lvl;
-    
-    node* obt;
-    taken_list_elem *t, *runner, *chosen;
-    
-    scelta_lvl = log2_(MAX_ALLOCABLE_BYTE/MIN_ALLOCABLE_BYTES);
-    tentativi = ops[myid] = 10000000 / number_of_processes ;
-    kept = number_of_leaves/number_of_processes;
-    i = j = 0;
-    
-    //printf("[%u] pid=%u tentativi=%u ops=%llu\n", myid, mypid, tentativi, ops[myid]);
-    
-    for(i=0;i<tentativi;i++){
+	unsigned int i, j, tentativi, kept;
+	unsigned int scelta;
+	unsigned int scelta_lvl;
+	
+	node* obt;
+	taken_list_elem *t, *runner, *chosen;
+	
+	scelta_lvl = log2_(MAX_ALLOCABLE_BYTE/MIN_ALLOCABLE_BYTES);
+	tentativi = ops[myid] = 10000000 / number_of_processes ;
+	kept = number_of_leaves/number_of_processes;
+	i = j = 0;
+	
+	//printf("[%u] pid=%u tentativi=%u ops=%llu\n", myid, mypid, tentativi, ops[myid]);
+	
+	for(i=0;i<tentativi;i++){
 		scelta = rand();
-        //ALLOC
-        if(scelta >=((RAND_MAX/10)*5)){
-        //if(scelta > ((RAND_MAX/10)*takenn->number)){
-            if(takenn->number == kept) break;            
-            //QUA CON SCELTA VIENE DECISO IL NUMERO DELLE PAGINE DA ALLOCARE
-            scelta = (MIN_ALLOCABLE_BYTES) << (rand_lim(scelta_lvl)); //<<rimpiazza con un exp^(-1)
-            //if(scelta==0)
-                scelta=1;
-            
-            if(takenn_serbatoio->number==0){
-                printf("Allocazioni %llu free %llu allocazioni-free=%llu\n", allocs[myid], frees[myid], allocs[myid] - frees[myid]);
-                exit(0);
-            }
-            obt = request_memory(scelta);
-            if (obt==NULL){
-                failures[myid]++;
-                continue;
-            }
-            allocs[myid]++;
-            
-            //estraggo un nodo dal serbatoio
-            t = takenn_serbatoio->head;
-            takenn_serbatoio->head = takenn_serbatoio->head->next;
-            takenn_serbatoio->number--;
-            //inserisco il nodo tra quelli presi
-            t->elem = obt;
-            t->next = takenn->head;;
-            takenn->head = t;
-            takenn->number++;
+		//ALLOC
+		if(scelta >=((RAND_MAX/10)*5)){
+		//if(scelta > ((RAND_MAX/10)*takenn->number)){
+			if(takenn->number == kept) break;			
+			//QUA CON SCELTA VIENE DECISO IL NUMERO DELLE PAGINE DA ALLOCARE
+			scelta = (MIN_ALLOCABLE_BYTES) << (rand_lim(scelta_lvl)); //<<rimpiazza con un exp^(-1)
+			//if(scelta==0)
+				scelta=1;
+			
+			if(takenn_serbatoio->number==0){
+				printf("Allocazioni %llu free %llu allocazioni-free=%llu\n", allocs[myid], frees[myid], allocs[myid] - frees[myid]);
+				exit(0);
+			}
+			obt = request_memory(scelta);
+			if (obt==NULL){
+				failures[myid]++;
+				continue;
+			}
+			allocs[myid]++;
+			
+			//estraggo un nodo dal serbatoio
+			t = takenn_serbatoio->head;
+			takenn_serbatoio->head = takenn_serbatoio->head->next;
+			takenn_serbatoio->number--;
+			//inserisco il nodo tra quelli presi
+			t->elem = obt;
+			t->next = takenn->head;;
+			takenn->head = t;
+			takenn->number++;
 		}
-        //FREE
-        else{
-            if(takenn->number==0){
+		//FREE
+		else{
+			if(takenn->number==0){
 				//printf("WTF\n");
 				i--; //le allocazioni non fatte non contano...
-                continue;
-            }
-            frees[myid]++;
+				continue;
+			}
+			frees[myid]++;
 
-            //scelgo il nodo da liberare nella mia taken list
-            scelta = rand_lim((takenn->number)-1); //ritorna un numero da 0<head> a number-1<ultimo>
-            
-            if(scelta==0){
-		        free_node(takenn->head->elem);
-                chosen = takenn->head;
-                takenn->head = takenn->head->next;
-            }
-            else{
+			//scelgo il nodo da liberare nella mia taken list
+			scelta = rand_lim((takenn->number)-1); //ritorna un numero da 0<head> a number-1<ultimo>
+			
+			if(scelta==0){
+				free_node(takenn->head->elem);
+				chosen = takenn->head;
+				takenn->head = takenn->head->next;
+			}
+			else{
 				runner = takenn->head;
-                for(j=0;j<scelta-1;j++)
-                    runner = runner->next;
-                
-                chosen = runner->next;
-                free_node(chosen->elem);
-                runner->next = runner->next->next;
-            }
-            takenn->number--;
-            chosen->next = takenn_serbatoio->head;
-            takenn_serbatoio->head = chosen;
-            takenn_serbatoio->number++;
-        }
-    }
-    //__asm__ __volatile__ ("mfence" ::: "memory");
-    //printf("[%u] FINI:%u %llu\n", myid, i, ops[myid]);
-     
+				for(j=0;j<scelta-1;j++)
+					runner = runner->next;
+				
+				chosen = runner->next;
+				free_node(chosen->elem);
+				runner->next = runner->next->next;
+			}
+			takenn->number--;
+			chosen->next = takenn_serbatoio->head;
+			takenn_serbatoio->head = chosen;
+			takenn_serbatoio->number++;
+		}
+	}
+	//__asm__ __volatile__ ("mfence" ::: "memory");
+	//printf("[%u] FINI:%u %llu\n", myid, i, ops[myid]);
+	 
 }
 
 
 /*
 void parallel_try(){
-    int i, j, allocazioni, free, tentativi;
-    unsigned long scelta;
-    unsigned int scelta_lvl;
-    
-    node* obt;
-    taken_list_elem *t, *runner, *chosen;
-    
-    scelta_lvl = log2_(MAX_ALLOCABLE_BYTE/MIN_ALLOCABLE_BYTES);
-    tentativi = 10000000 / number_of_processes ;
-    i = j = free = allocazioni = 0;
-    
-    for(i=0;i<tentativi;i++){
+	int i, j, allocazioni, free, tentativi;
+	unsigned long long scelta;
+	unsigned int scelta_lvl;
+	
+	node* obt;
+	taken_list_elem *t, *runner, *chosen;
+	
+	scelta_lvl = log2_(MAX_ALLOCABLE_BYTE/MIN_ALLOCABLE_BYTES);
+	tentativi = 10000000 / number_of_processes ;
+	i = j = free = allocazioni = 0;
+	
+	for(i=0;i<tentativi;i++){
 		
-        scelta = rand();
-        
-        //ALLOC
-        //if(scelta >=((RAND_MAX/10)*5)){
-        if(scelta >= ((RAND_MAX/10)*takenn->number)){
-            
-            //QUA CON SCELTA VIENE DECISO IL NUMERO DELLE PAGINE DA ALLOCARE
-            scelta = (MIN_ALLOCABLE_BYTES) << (rand_lim(scelta_lvl)); //<<rimpiazza con un exp^(-1)
-            //if(scelta==0)
-                scelta=1;
-            
-            if(takenn_serbatoio->number==0){
-                printf("Allocazioni %d free %d allocazioni-free=%d\n", allocazioni, free, allocazioni-free);
-                exit(0);
-            }
-            
-            obt = request_memory(scelta);
-            if (obt==NULL){
-                failures[write_failures_on]++;
-                continue;
-            }
-            allocazioni++;
-            
-            //estraggo un nodo dal serbatoio
-            t = takenn_serbatoio->head;
-            takenn_serbatoio->head = takenn_serbatoio->head->next;
-            takenn_serbatoio->number--;
-            //inserisco il nodo tra quelli presi
-            t->elem = obt;
-            t->next = takenn->head;;
-            takenn->head = t;
-            takenn->number++;
-        }
-        //FREE
-        else{
-            if(takenn->number==0){
-                continue;
-            }
-            free++;
+		scelta = rand();
+		
+		//ALLOC
+		//if(scelta >=((RAND_MAX/10)*5)){
+		if(scelta >= ((RAND_MAX/10)*takenn->number)){
+			
+			//QUA CON SCELTA VIENE DECISO IL NUMERO DELLE PAGINE DA ALLOCARE
+			scelta = (MIN_ALLOCABLE_BYTES) << (rand_lim(scelta_lvl)); //<<rimpiazza con un exp^(-1)
+			//if(scelta==0)
+				scelta=1;
+			
+			if(takenn_serbatoio->number==0){
+				printf("Allocazioni %d free %d allocazioni-free=%d\n", allocazioni, free, allocazioni-free);
+				exit(0);
+			}
+			
+			obt = request_memory(scelta);
+			if (obt==NULL){
+				failures[write_failures_on]++;
+				continue;
+			}
+			allocazioni++;
+			
+			//estraggo un nodo dal serbatoio
+			t = takenn_serbatoio->head;
+			takenn_serbatoio->head = takenn_serbatoio->head->next;
+			takenn_serbatoio->number--;
+			//inserisco il nodo tra quelli presi
+			t->elem = obt;
+			t->next = takenn->head;;
+			takenn->head = t;
+			takenn->number++;
+		}
+		//FREE
+		else{
+			if(takenn->number==0){
+				continue;
+			}
+			free++;
 
-            //scelgo il nodo da liberare nella mia taken list
-            scelta = rand_lim((takenn->number)-1); //ritorna un numero da 0<head> a number-1<ultimo>
-            
-            if(scelta==0){
-                free_node(takenn->head->elem);
-                chosen = takenn->head;
-                takenn->head = takenn->head->next;
-            }
-            else{
+			//scelgo il nodo da liberare nella mia taken list
+			scelta = rand_lim((takenn->number)-1); //ritorna un numero da 0<head> a number-1<ultimo>
+			
+			if(scelta==0){
+				free_node(takenn->head->elem);
+				chosen = takenn->head;
+				takenn->head = takenn->head->next;
+			}
+			else{
 				runner = takenn->head;
-                for(j=0;j<scelta-1;j++)
-                    runner = runner->next;
-                
-                chosen = runner->next;
-                free_node(chosen->elem);
-                runner->next = runner->next->next;
-            }
-            takenn->number--;
-            chosen->next = takenn_serbatoio->head;
-            takenn_serbatoio->head = chosen;
-            takenn_serbatoio->number++;
-        }
-    } 
+				for(j=0;j<scelta-1;j++)
+					runner = runner->next;
+				
+				chosen = runner->next;
+				free_node(chosen->elem);
+				runner->next = runner->next->next;
+			}
+			takenn->number--;
+			chosen->next = takenn_serbatoio->head;
+			takenn_serbatoio->head = chosen;
+			takenn_serbatoio->number++;
+		}
+	} 
 }
 */
 int main(int argc, char**argv){
-	printf("That's me!\n");
-    int status, local_pid;
-    
-    if(argc!=3){
-        printf("usage: ./a.out <number of threads> <number of levels>\n");
-        exit(0);
-    }
-    
-#ifdef NUMA
-    if(numa_available()<0){
-        puts("numa is not available");
-        exit(-1);
-    }
-    number_of_numa_nodes = numa_max_node() + 1;
-    //printf("numa nodes are %d\n", number_of_numa_nodes);
-    //printf("%d\n", numa_available());
-#endif
-    
-    //Per scrivere solo una volta il risultato finale su file
-    master=getpid();
-    int i=0;
-
-    number_of_processes=atoi(argv[1]);
-    unsigned long requested = atol(argv[2]);
-    
-    init(requested);
-    
-    failures = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    allocs = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    frees = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    ops = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-        
-    for(i=0; i<number_of_processes; i++){
-        allocs[i] = 0;
-        frees[i] = 0;
-        failures[i] = 0;
-        ops[i] = 0;
-    }
-    
-    for(i=0; i<number_of_processes; i++){
-        if(fork()==0){
-            //child code, do work and exit.
-            mypid = getpid();//__sync_fetch_and_add(id_counter, 1);//
-            myid = mypid % number_of_processes;
-            
-            //write_failures_on = mypid % number_of_processes;
-            //per la gestione dei nodi presi dal singolo processo.
-            takenn = malloc(sizeof(taken_list));
-            takenn->head = NULL;
-            takenn->number = 0;
-            
-            takenn_serbatoio = malloc(sizeof(taken_list));
-            takenn_serbatoio->number = SERBATOIO_DIM;
-            takenn_serbatoio->head = malloc(sizeof(taken_list_elem));
-            taken_list_elem* runner = takenn_serbatoio->head;
-            int j;
-            for(j=0;j<SERBATOIO_DIM;j++){
-                runner->next = malloc(sizeof(taken_list_elem));
-                runner = runner->next;
-            }
-            
-            parallel_try();
-            
-            free(takenn);
-            exit(0);
-        }
-        //else parent spawn processes
-    }
-    
-    //only parent reach this. Wait all processes (-1 parameters) and terminate
-    while ( (local_pid = waitpid(-1, &status, 0)) ){
-	if(!WIFEXITED(status)) printf("Figlio %d uscito per errore\n", local_pid);
-    //printf("Il figlio %d è terminato\n", local_pid);
-        if (errno == ECHILD) {
-			break;
-        }
+	
+	int status, local_pid;
+	
+	if(argc!=3){
+		printf("usage: ./a.out <number of threads> <number of levels>\n");
+		exit(0);
 	}
-       
-    //__asm__ __volatile__ ("mfence" ::: "memory");
-    unsigned long long total_fail = 0, total_alloc = 0, total_free = 0, total_ops = 0;
-    
-    printf("_______________________________________\n");
-    for(i=0;i<number_of_processes;i++){
-		if(i>0) printf(". . . . . . . . . . . . . . . \n");
-        printf("Process %d: TOT_OPS %llu\n",i, ops[i]);
-        printf("\t allocati: %llu\n", allocs[i]);
-        printf("\t dealloca: %llu\n", frees[i]);
-        printf("\t     diff: %llu\n", allocs[i]-frees[i]);
-        printf("\t failures: %llu\n", failures[i]);
-        printf("\t tot_ops_done: %llu\n", allocs[i]+frees[i]);
-        total_fail += failures[i];
-        total_alloc += allocs[i];
-        total_free += frees[i];
-        total_ops += ops[i];
-    }
-    printf("_______________________________________\n");
-    printf("Total ops         %llu\n", total_ops);
-    printf("total allocs is   %llu\n", total_alloc);
-    printf("total frees is    %llu\n", total_free);
-    printf("total failures is %llu\n", total_fail);
-    printf("total operations: %llu\n", total_alloc + total_free + total_fail);
-    //write_on_a_file_in_ampiezza();
-#ifdef DEBUG
-    printf("total nodes allocated: %llu\n", *node_allocated);
-    printf("total memory allocated: %llu Bytes\n", *size_allocated);
+	
+#ifdef NUMA
+	if(numa_available()<0){
+		puts("numa is not available");
+		exit(-1);
+	}
+	number_of_numa_nodes = numa_max_node() + 1;
+	//printf("numa nodes are %d\n", number_of_numa_nodes);
+	//printf("%d\n", numa_available());
 #endif
-    
-    return 0;
+	
+	//Per scrivere solo una volta il risultato finale su file
+	master=getpid();
+	int i=0;
+
+	number_of_processes=atoi(argv[1]);
+	unsigned long long requested = atol(argv[2]);
+	
+	init(requested);
+	printf("_______\n");
+	write_on_a_file_in_ampiezza_start();
+	
+	failures = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	allocs = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	frees = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	ops = mmap(NULL, sizeof(unsigned long long) * number_of_processes, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+		
+	for(i=0; i<number_of_processes; i++){
+		allocs[i] = 0;
+		frees[i] = 0;
+		failures[i] = 0;
+		ops[i] = 0;
+	}
+	
+	for(i=0; i<number_of_processes; i++){
+		if(fork()==0){
+			//child code, do work and exit.
+			mypid = getpid();//__sync_fetch_and_add(id_counter, 1);//
+			myid = mypid % number_of_processes;
+			printf("ID: %d\n", myid);			
+			
+			//write_failures_on = mypid % number_of_processes;
+			//per la gestione dei nodi presi dal singolo processo.
+			takenn = malloc(sizeof(taken_list));
+			takenn->head = NULL;
+			takenn->number = 0;
+			
+			takenn_serbatoio = malloc(sizeof(taken_list));
+			takenn_serbatoio->number = SERBATOIO_DIM;
+			takenn_serbatoio->head = malloc(sizeof(taken_list_elem));
+			taken_list_elem* runner = takenn_serbatoio->head;
+			int j;
+			for(j=0;j<SERBATOIO_DIM;j++){
+				runner->next = malloc(sizeof(taken_list_elem));
+				runner = runner->next;
+			}
+			
+			parallel_try();
+			
+			free(takenn);
+			exit(0);
+		}
+		//else parent spawn processes
+	}
+	
+	//only parent reach this. Wait all processes (-1 parameters) and terminate
+	while ( (local_pid = waitpid(-1, &status, 0)) ){
+	if(!WIFEXITED(status)) printf("Figlio %d uscito per errore\n", local_pid);
+	//printf("Il figlio %d è terminato\n", local_pid);
+		if (errno == ECHILD) {
+			break;
+		}
+	}
+	   
+	//__asm__ __volatile__ ("mfence" ::: "memory");
+	unsigned long long total_fail = 0, total_alloc = 0, total_free = 0, total_ops = 0;
+	
+	printf("_______________________________________\n");
+	for(i=0;i<number_of_processes;i++){
+		if(i>0) printf(". . . . . . . . . . . . . . . \n");
+		printf("Process %d: TOT_OPS %llu\n",i, ops[i]);
+		printf("\t allocati: %llu\n", allocs[i]);
+		printf("\t dealloca: %llu\n", frees[i]);
+		printf("\t	 diff: %llu\n", allocs[i]-frees[i]);
+		printf("\t failures: %llu\n", failures[i]);
+		printf("\t tot_ops_done: %llu\n", allocs[i]+frees[i]);
+		total_fail += failures[i];
+		total_alloc += allocs[i];
+		total_free += frees[i];
+		total_ops += ops[i];
+	}
+	printf("_______________________________________\n");
+	printf("Total ops		 %llu\n", total_ops);
+	printf("total allocs is   %llu\n", total_alloc);
+	printf("total frees is	%llu\n", total_free);
+	printf("total failures is %llu\n", total_fail);
+	printf("total operations: %llu\n", total_alloc + total_free + total_fail);
+	//write_on_a_file_in_ampiezza();
+#ifdef DEBUG
+	printf("total nodes allocated: %llu\n", *node_allocated);
+	printf("total memory allocated: %llu Bytes\n", *size_allocated);
+	printf("Leaves really allocated: %u\n", count_occupied_leaves());
+	
+	//if(*node_allocated < number_of_leaves)
+		write_on_a_file_in_ampiezza();
+#endif
+	
+	return 0;
 }
 
 
 /*
-    SCRIVE SU FILE I NODI PRESI DA UN THREAD - FUNZIONE PRETTAMENTE DI DEBUG
+	SCRIVE SU FILE I NODI PRESI DA UN THREAD - FUNZIONE PRETTAMENTE DI DEBUG
  */
  /*
 void write_taken(){
-    char filename[128];
-    sprintf(filename, "./debug/taken_%d.txt", getpid()%number_of_processes);
-    FILE *f = fopen(filename, "w");
-    unsigned i;
-    
-    if (f == NULL){
-        printf("Error opening file!\n");
-        exit(1);
-    }
-    
-    taken_list_elem* runner = takenn->head;
-    
-    // print some text 
-    for(i=0;i<takenn->number;i++){
-        fprintf(f, "%u\n", runner->elem->pos);
-        runner=runner->next;
-    }
-    fclose(f);   
+	char filename[128];
+	sprintf(filename, "./debug/taken_%d.txt", getpid()%number_of_processes);
+	FILE *f = fopen(filename, "w");
+	unsigned i;
+	
+	if (f == NULL){
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	
+	taken_list_elem* runner = takenn->head;
+	
+	// print some text 
+	for(i=0;i<takenn->number;i++){
+		fprintf(f, "%u\n", runner->elem->pos);
+		runner=runner->next;
+	}
+	fclose(f);   
 }
 */
--- 4lvl/nballoc.h
+++ 4lvl/nballoc.h
@@ -21,44 +21,47 @@
 typedef struct _node node;
 
 typedef struct node_container_{
-    unsigned long nodes;
-    node* bunch_root;
+	unsigned long long nodes;
+	node* bunch_root;
 }node_container;
 
 struct _node{
-    unsigned long mem_start; //spiazzamento all'interno dell'overall_memory
-    unsigned long mem_size;
-    node_container* container;
-    unsigned pos; //posizione all'interno dell'array "tree"
+	unsigned long long mem_start; //spiazzamento all'interno dell'overall_memory
+	unsigned long long mem_size;
+	node_container* container;
+	unsigned pos; //posizione all'interno dell'array "tree"
 #ifdef NUMA
-    unsigned numa_node; //to remember to which tree the node belongs to
+	unsigned int numa_node; //to remember to which tree the node belongs to
 #endif
-    char container_pos; //posizione all'interno del rispettivo container (1-15)
+	unsigned int container_pos; //posizione all'interno del rispettivo container (1-15)
 };
 
 
 
 typedef struct _taken_list_elem{
-    struct _taken_list_elem* next;
-    node* elem;
+	struct _taken_list_elem* next;
+	node* elem;
 }taken_list_elem;
 
 typedef struct _taken_list{
-    struct _taken_list_elem* head;
-    unsigned number;
+	struct _taken_list_elem* head;
+	unsigned int number;
 }taken_list;
 
 
-extern unsigned mypid;
-extern unsigned myid;
-extern unsigned master;
+extern unsigned int mypid;
+extern unsigned int myid;
+extern unsigned int master;
 extern unsigned int number_of_leaves;
 
 
-void init(unsigned long memory);
+void init(unsigned long long memory);
 void free_node(node* n);
-node* request_memory(unsigned pages);
+node* request_memory(unsigned int bytes);
 void destroy();
+extern unsigned int count_occupied_leaves();
+extern void write_on_a_file_in_ampiezza();
+extern void write_on_a_file_in_ampiezza_start();
 
 #ifdef DEBUG
 extern unsigned long long *node_allocated, *size_allocated;
