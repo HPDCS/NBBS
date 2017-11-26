e5b91cbe1ee6216bbac3c858d536d085993982d0
RomoloMarotta <romolo.marotta@gmail.com>
2017-11-26 18:30:34 +0100

    New test script

--- launch_test.sh
+++ launch_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 THREAD_list="8"		#numero di thread
-RUN_list="1"					#lista del numero di run
-ALLOC_list="4lvl-nb" #"4lvl-nb 1lvl-nb" #"hoard 4lvl-nb 1lvl-nb 4lvl-sl 1lvl-sl ptmalloc3 libc "
-SIZE_list="8"
+RUN_list="1 4 8 16 32"					#lista del numero di run
+ALLOC_list="4lvl-nb 1lvl-nb" #"hoard 4lvl-nb 1lvl-nb 4lvl-sl 1lvl-sl ptmalloc3 libc "
+SIZE_list="8 128"
 
 
 MAX_RETRY="10"
@@ -25,9 +25,9 @@ do
 		do
 			for threads in $THREAD_list
 			do
-				EX1="time ./benchmarks/TB_linux-scalability/TB_linux-scalability-$alloc $threads $size"
-				EX2="time ./benchmarks/TB_threadtest/TB_threadtest-$alloc $threads $size"
-				EX3="time ./benchmarks/larson/larson-$alloc 10 `echo $((size-1))` $size 1000 10000 1 $threads"
+				EX1="time -f Real:%e,User:%U,Sys:%S,PCPU:%P,PFAULT:%F,MEM:%K ./benchmarks/TB_linux-scalability/TB_linux-scalability-$alloc $threads $size 			"
+				EX2="time -f Real:%e,User:%U,Sys:%S,PCPU:%P,PFAULT:%F,MEM:%K ./benchmarks/TB_threadtest/TB_threadtest-$alloc $threads $size 						"
+				EX3="time -f Real:%e,User:%U,Sys:%S,PCPU:%P,PFAULT:%F,MEM:%K ./benchmarks/larson/larson-$alloc 10 `echo $((size-1))` $size 1000 10000 1 $threads 	"
 				OUT1="${FOLDER}/TBLS-$alloc-sz$size-TH$threads-R$run"; touch $OUT1
 				OUT2="${FOLDER}/TBTT-$alloc-sz$size-TH$threads-R$run"; touch $OUT2
 				OUT3="${FOLDER}/LRSN-$alloc-sz$size-TH$threads-R$run"; touch $OUT3
