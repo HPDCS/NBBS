2827ba5aa6cea322ad753eefc978055e7b9e5c65
Romolo <romolo.marotta@gmail.com>
2019-09-24 22:27:47 +0200

    added freemap in utils

--- utils/utils.c
+++ utils/utils.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+__thread unsigned int freemap[128];
+
 unsigned int rand_lim(unsigned int limit) {
     /* return a random number between 0 and limit inclusive.
      */
--- utils/utils.h
+++ utils/utils.h
@@ -5,4 +5,28 @@ unsigned int rand_lim(unsigned int limit);
 unsigned int upper_power_of_two(unsigned int v);
 unsigned int log2_ (unsigned long value);
 
+#define ENABLE_CACHE 1
+
+extern __thread unsigned int freemap[];
+
+static inline void update_freemap(unsigned int key, unsigned int value){
+    unsigned int tmp = 
+  #if ENABLE_CACHE == 1
+    freemap[key];
+  #else
+    0;
+  #endif
+//    if(tmp == 0 || value < tmp) 
+freemap[key] = value;
+}
+
+static inline unsigned int get_freemap(unsigned int key, unsigned int max){
+  #if ENABLE_CACHE == 0
+     return 0;
+  #endif
+     unsigned int tmp = freemap[key];
+     freemap[key] += freemap[key] != 0;
+     freemap[key] = (-(freemap[key]<max)) & freemap[key];
+     return tmp;
+}
 #endif
