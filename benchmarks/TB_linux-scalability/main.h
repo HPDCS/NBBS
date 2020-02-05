471308498baf8825378d0608d0b1e5d84ebfaad3
Romolo <romolo.marotta@gmail.com>
2020-02-05 04:02:53 +0100

    [Benchs] Restored original linux-scalability

--- benchmarks/TB_linux-scalability/main.h
+++ benchmarks/TB_linux-scalability/main.h
@@ -12,25 +12,35 @@ void  bd_xx_free(FREE_GET_PAR(void*,size_t));
 #include "parameters.h"
 
 void linux_scalability(ALLOC_GET_PAR(unsigned long long fixed_size, unsigned int fixed_order), unsigned long long *allocs, unsigned long long *failures, unsigned long long *frees){
-	unsigned int i, tentativi;
-	#if KERNEL_BD == 0
+	unsigned int i, tentativi = LS_ITERATIONS;
+#if KERNEL_BD == 0
 	void *obt, *cmp = NULL;
-	#else
-	unsigned long long obt, cmp = 0ULL;
-	#endif
+	void **addrs = malloc(sizeof(void*)*tentativi);
+#else
+	unsigned long long cmp = 0ULL;
+	unsigned long long *addrs = vmalloc(sizeof(void*)*tentativi);
+#endif
 	
-	tentativi = LS_ITERATIONS; // / number_of_processes ;
 	i = 0;
 
 	for(i=0;i<tentativi;i++){
-		obt = TO_BE_REPLACED_MALLOC(ALLOC_GET_PAR(fixed_size, fixed_order));
-		if (obt==cmp){
+		addrs[i] = TO_BE_REPLACED_MALLOC(ALLOC_GET_PAR(fixed_size, fixed_order));
+		if (addrs[i] == cmp){
 			(*failures)++;
-			continue;
 		}
-
-		(*allocs)++;
-		TO_BE_REPLACED_FREE(FREE_GET_PAR(obt, fixed_order));
-		(*frees)++;
+		else
+			(*allocs)++;
 	}
+	
+	for(i=0;i<tentativi;i++){
+		if(addrs[i] != cmp){
+			TO_BE_REPLACED_FREE(FREE_GET_PAR(addrs[i], fixed_order));
+			(*frees)++;
+		}
+	}
+#if KERNEL_BD == 0
+	free(addrs);
+#else
+	vfree(addrs);
+#endif
 }
--- benchmarks/TB_linux-scalability/parameters.h
+++ benchmarks/TB_linux-scalability/parameters.h
@@ -1,7 +1,6 @@
 #ifndef __LS_PARAMETERS__
 #define __LS_PARAMETERS__
 
-#define LS_ITERATIONS	50000000ULL
-#define LS_LEVELS		5ULL
+#define LS_ITERATIONS	50000ULL
 
 #endif
